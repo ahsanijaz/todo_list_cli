e885b478d3ccd4ffa46495be35b5970c3bd0fc62
ahsanijaz <ahsan.ijaz3@gmail.com>
2025-10-07 23:33:00 +0800

    added storage

---
+++ src/storage.rs
@@ -0,0 +1,22 @@
+// src/storage.rs
+
+use crate::tasks::TodoList;
+use std::fs;
+use std::io::{self, Write};
+
+pub fn save_tasks(file_path: &str, todo_list: &TodoList) -> io::Result<()> {
+    let mut file = fs::File::create(file_path)?;
+    for task in &todo_list.tasks {
+        writeln!(file, "{}", task)?;
+    }
+    Ok(())
+}
+
+pub fn load_tasks(file_path: &str) -> io::Result<TodoList> {
+    let mut todo_list = TodoList::new();
+    let content = fs::read_to_string(file_path)?;
+    for line in content.lines() {
+        todo_list.add_task(line.to_string());
+    }
+    Ok(todo_list)
+}
---
+++ src/task.rs
@@ -0,0 +1,27 @@
+// src/tasks.rs
+
+pub struct TodoList {
+    tasks: Vec<String>,
+}
+
+impl TodoList {
+    pub fn new() -> TodoList {
+        TodoList { tasks: Vec::new() }
+    }
+
+    pub fn add_task(&mut self, task: String) {
+        self.tasks.push(task);
+    }
+
+    pub fn complete_task(&mut self, index: usize) {
+        if index < self.tasks.len() {
+            self.tasks[index] = format!("[x] {}", self.tasks[index]);
+        }
+    }
+
+    pub fn list_tasks(&self) {
+        for (index, task) in self.tasks.iter().enumerate() {
+            println!("{}: {}", index, task);
+        }
+    }
+}
